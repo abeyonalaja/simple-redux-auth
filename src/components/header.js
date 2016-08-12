504a270a4c0d6495578caa49321880a6d1b9ef8f
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-12 13:26:57 +0100

    Create header component

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render () {
+    return (
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            sign in
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Header;
