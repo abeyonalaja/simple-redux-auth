d0db950700c27edc305a2fd0bc710dbdfe54a229
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-13 22:00:47 +0100

    Create stub for sign up component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions/';
+
+class SignUp extends Component {
+  render () {
+    return(
+      <div>SignUp</div>
+    );
+  }
+};
+
+export default SignUp;
