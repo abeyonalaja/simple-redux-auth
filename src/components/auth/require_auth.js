2d3213d1ca763bf7255b2d62f50d6647d80702cb
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-14 13:00:16 +0100

    Create higher order component

---
+++ src/components/auth/require_auth.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function (CompsoedComponent) {
+
+  class Authentication extends Component {
+
+    static contextTypes = {
+      router : React.PropTypes.object
+    }
+
+    componentWillMount() {
+
+      if( !this.porps.authenticated ) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate( nextProps ) {
+      if( !this.props.authenticated ){
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return <CompsoedComponent { ...this.props } />
+    }
+  }
+
+  function mapStateToProps( state ) {
+    return { authenticated : state.authenticated };
+  }
+
+  return connect( mapStateToProps )( Authentication );
+}
